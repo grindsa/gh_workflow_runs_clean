b483c7fdfc1f666eb2c14b3ac1f1ea06f202ccf5
grindsa <grindelsack@gmail.com>
2023-10-14 06:20:11 +0200

    [fix] special handling for scheduled jobs

--- gh_workflow_runs_delete.py
+++ gh_workflow_runs_delete.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from dateutil.parser import parse
 import requests
 
+
 def arg_parse():
     """ simple argparser """
     parser = argparse.ArgumentParser(description='gh_workflow_runs_delete.py - delete github action logs and artifacts')
@@ -30,7 +31,7 @@ def arg_parse():
     if not reponame:
         print('reponame (-r) is missing')
         sys.exit(0)
-    if  username and token:
+    if username and token:
         auth = (username, token)
     else:
         print('authentication incomplete (either user or token are missing)')
@@ -38,61 +39,71 @@ def arg_parse():
 
     return (debug, auth, reponame, branchlist, commit_num)
 
+
 def branchlist_get(debug, auth, reponame):
     """ get list of branches from github """
     print_debug(debug, 'branchlist_get()')
-    branch_list = []
-    url = 'https://api.github.com/repos/{0}/branches'.format(reponame)
-    resp = requests.get(url=url, auth=auth)
+    branch_list = ['scheduled']
+    url = f'https://api.github.com/repos/{reponame}/branches'
+    resp = requests.get(url=url, auth=auth, timeout=20)
     for branch in resp.json():
         if 'name' in branch:
             branch_list.append(branch['name'])
 
     return branch_list
 
+
 def wfruns_get(debug, auth, reponame):
     """ get list of runs """
     print_debug(debug, 'wfruns_get()')
     perpage = 100
-    url = 'https://api.github.com/repos/{0}/actions/runs?per_page={1}'.format(reponame, perpage)
-    resp = requests.get(url=url, auth=auth)
+    url = f'https://api.github.com/repos/{reponame}/actions/runs?per_page={perpage}'
+    resp = requests.get(url=url, auth=auth, timeout=20)
 
     pagenum = 0
     if 'total_count' in resp.json():
-        pagenum = math.ceil(resp.json()['total_count']/perpage)
-        print_debug(debug, 'totalcount: {0}, perpage: {1}, pages: {2}'.format(resp.json()['total_count'], perpage, pagenum))
+        pagenum = math.ceil(resp.json()['total_count'] / perpage)
+        print_debug(debug, f'totalcount: {resp.json()["total_count"]}, perpage: {perpage}, pages: {pagenum}')
 
     workflow_list = []
     if pagenum:
-        for ele in range(1, pagenum+1):
-            print_debug(debug, 'fetching page: {0}'.format(ele))
-            url = 'https://api.github.com/repos/{0}/actions/runs?per_page={1}&page={2}'.format(reponame, perpage, ele)
-            resp = requests.get(url=url, auth=auth)
+        for ele in range(1, pagenum + 1):
+            print_debug(debug, f'fetching page: {ele}')
+            url = f'https://api.github.com/repos/{reponame}/actions/runs?per_page={perpage}&page={ele}'
+            resp = requests.get(url=url, auth=auth, timeout=20)
             if 'workflow_runs' in resp.json():
                 workflow_list.extend(resp.json()['workflow_runs'])
 
     # json_store('ids.json', workflow_list)
     return workflow_list
 
+
 def wfruns_group(debug, action_list):
     """ group action list by branch """
     print_debug(debug, 'wfruns_group()')
-    actions_dic = {}
+    actions_dic = {'scheduled': {}}
     for workflow in action_list:
         if 'head_branch' in workflow and 'head_sha' in workflow and 'head_commit' in workflow and 'id' in workflow:
             uts = int(calendar.timegm(parse(workflow['head_commit']['timestamp']).timetuple()))
             # print(workflow['id'], workflow['head_branch'], workflow['head_sha'], workflow['head_commit']['timestamp'], uts)
-
             # add branchname to dictionary
             if workflow['head_branch'] not in actions_dic:
                 actions_dic[workflow['head_branch']] = {}
 
-            # add uts to branch-tree
-            if uts not in actions_dic[workflow['head_branch']]:
-                actions_dic[workflow['head_branch']][uts] = {'commit': workflow['head_sha'], 'id_list': []}
-            actions_dic[workflow['head_branch']][uts]['id_list'].append(workflow['id'])
+            if workflow['event'] == 'schedule':
+                # special handling for scheduled workflows
+                cdate, _junk = workflow['created_at'].split('T', 1)
+                if cdate not in actions_dic['scheduled']:
+                    actions_dic['scheduled'][cdate] = {'commit': workflow['head_sha'], 'id_list': []}
+                actions_dic['scheduled'][cdate]['id_list'].append(workflow['id'])
+            else:
+                # add uts to branch-tree
+                if uts not in actions_dic[workflow['head_branch']]:
+                    actions_dic[workflow['head_branch']][uts] = {'commit': workflow['head_sha'], 'id_list': []}
+                actions_dic[workflow['head_branch']][uts]['id_list'].append(workflow['id'])
 
     return actions_dic
+
 
 def json_load(file_name):
     """ load json structure from file """
@@ -100,19 +111,22 @@ def json_load(file_name):
         data = json.load(json_file)
     return data
 
+
 def json_store(file_name_, data_):
     """ store structure as json to file """
     with open(file_name_, 'w', encoding='utf-8') as out_file:
         json.dump(data_, out_file, ensure_ascii=False, indent=4)
 
+
 def print_debug(debug, text):
     """ little helper to print debug messages """
     if debug:
-        print('{0}: {1}'.format(datetime.now(), text))
+        print(f'{datetime.now()}: {text}')
+
 
 def idlist_filter(debug, action_dic, branch_list, commit_number):
     """ select ids to be deleted """
-    print_debug(debug, 'idlist_filter({0})'.format(commit_number))
+    print_debug(debug, f'idlist_filter({commit_number})')
     id_list = []
     for branch in action_dic:
         delete = False
@@ -129,13 +143,15 @@ def idlist_filter(debug, action_dic, branch_list, commit_number):
                     id_list.append(id_)
     return id_list
 
+
 def idlist_delete(debug, auth, reponame, id_list):
     """ delete worflow logs """
-    print_debug(debug, 'idlist_delete({0})'.format(len(id_list)))
+    print_debug(debug, f'idlist_delete({id_list})')
     for id_ in id_list:
-        print_debug(debug, 'delete id: {0}'.format(id_))
-        url = 'https://api.github.com/repos/{0}/actions/runs/{1}'.format(reponame, id_)
-        resp = requests.delete(url=url, auth=auth)
+        print_debug(debug, f'delete id: {id_}')
+        url = f'https://api.github.com/repos/{reponame}/actions/runs/{id_}'
+        _resp = requests.delete(url=url, auth=auth, timeout=20)
+
 
 if __name__ == '__main__':
 
@@ -143,10 +159,13 @@ if __name__ == '__main__':
 
     if not BRANCHLIST:
         BRANCH_LIST = branchlist_get(DEBUG, AUTH, REPONAME)
+
     # print(BRANCH_LIST)
     if BRANCH_LIST:
         ACTION_LIST = wfruns_get(DEBUG, AUTH, REPONAME)
+
     # ACTION_LIST = json_load('foo.json')
+    # json_store('wf.json', ACTION_LIST)
 
     # group actions per branch
     ACTION_DIC = wfruns_group(DEBUG, ACTION_LIST)
